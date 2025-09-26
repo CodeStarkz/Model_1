9d94d83f30aa1842455649adb9e69edf92acf845
CodeStarkz <101527767+CodeStarkz@users.noreply.github.com>
2025-09-26 22:45:24 +0530

    First session over multi-threading

--- Python_Core_works/OOPs_Session/GPT_test.py
+++ Python_Core_works/OOPs_Session/GPT_test.py
@@ -29,3 +29,25 @@ class car():
 Benz=car("Mercidies","Suv",20)
 
 Benz.car_info()
+
+print("----------------------- -------------------------")
+
+class Father:
+    def __init__(self, house, f_name):
+        self.house = house
+        self.f_name = f_name
+class Mother:
+    def __init__(self, jewellery, m_name):
+        self.jewellery = jewellery
+        self.m_name = m_name
+
+class Child(Father, Mother):
+    def __init__(self, house, jewellery, car,f_name, m_name):
+        Father.__init__(self, house, f_name)       # Explicit call
+        Mother.__init__(self, jewellery, m_name)   # Explicit call
+        self.car = car
+
+c = Child("Villa", "Gold", "BMW")
+print(c.house)       # Villa
+print(c.jewellery)   # Gold
+print(c.car)         # BMW
